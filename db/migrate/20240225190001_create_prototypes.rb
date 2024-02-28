1718cf3562c143eaf69d3ab29a8031800a842eb2
Shoki-darou <shoki.rugby0115@gmail.com>
2024-02-28 21:03:44 +0900

    デプロイの準備

--- db/migrate/20240225190001_create_prototypes.rb
+++ db/migrate/20240225190001_create_prototypes.rb
@@ -1,9 +1,9 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.string :title,     nill: false
-      t.text :catch_copy,  nill: false
-      t.text :concept,     nill: false
+      t.string :title,     null: false
+      t.text :catch_copy,  null: false
+      t.text :concept,     null: false
       t.references :user,  null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_26_152019) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_25_190042) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_26_152019) do
   end
 
   create_table "comments", charset: "utf8", force: :cascade do |t|
-    t.text "content"
+    t.text "content", null: false
     t.bigint "prototype_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -50,9 +50,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_26_152019) do
   end
 
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
-    t.string "title"
-    t.text "catch_copy"
-    t.text "concept"
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -78,4 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_26_152019) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
